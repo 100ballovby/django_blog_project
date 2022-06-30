57015b4186634fe3b383c8d6aad30f7714527d57
demidraksin <greatraksin@icloud.com>
2022-06-30 15:32:20 +0300

    Adding blog main page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'published']  # здесь указываются поля, которые вы хотите видеть в админке для этой модели
+    list_filter = ['created_at', 'published']  # здесь указываются фильтры в админке для этой модели
+
+
+admin.site.register(Post, PostAdmin)
