338f0718156afe2ead305a6a0919ea2084a27f76
demidraksin <greatraksin@icloud.com>
2022-06-30 16:04:50 +0300

    Adding pages

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def posts(request):
+    posts = Post.objects.filter(published=True).order_by('-created_at')
+    # ^ достаю все посты из базы данных и закидываю их в список
+    return render(request, 'index.html', {'posts': posts})
